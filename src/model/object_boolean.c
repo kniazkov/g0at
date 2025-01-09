0015f1e4ac7501e7b7a2540fd8d5bfefa2ee70d4
Ivan Kniazkov <ivan@kniazkov.com>
2025-01-09 14:03:42 +0300

    Boolean prototypes

--- src/model/object_boolean.c
+++ src/model/object_boolean.c
@@ -10,6 +10,76 @@
 #include "common_methods.h"
 
 /**
+ * @brief Retrieves all property keys from an object (stub implementation).
+ * 
+ * This is a stub implementation of the function to retrieve all keys of the properties 
+ * defined on an object. Currently, it returns an empty `object_array_t` as a placeholder.
+ * 
+ * @param obj The object from which to retrieve the keys.
+ * @return An empty `object_array_t` (placeholder implementation).
+ */
+static object_array_t get_keys(const object_t *obj) {
+    return (object_array_t){ NULL, 0 };
+}
+
+/**
+ * @brief Retrieves the value of a property from an object (stub implementation).
+ * 
+ * This is a stub implementation of the function to retrieve the value of a property from
+ * an object. Currently, it returns `NULL` as a placeholder.
+ * 
+ * @param obj The object from which to retrieve the property.
+ * @param key The key of the property to retrieve.
+ * @return Always returns `NULL` (placeholder implementation).
+ */
+static object_t *get_property(const object_t *obj, const object_t *key) {
+    return NULL;
+}
+
+/**
+ * @var boolean_proto_vtbl
+ * @brief Virtual table defining the behavior of the prototype boolean object.
+ */
+static object_vtbl_t boolean_proto_vtbl = {
+    .type = TYPE_STRING,
+    .inc_ref = stub_memory_function,
+    .dec_ref = stub_memory_function,
+    .mark = stub_memory_function,
+    .sweep = stub_memory_function,
+    .release = stub_memory_function,
+    .compare = compare_object_addresses,
+    .clone = clone_singleton,
+    .to_string = common_to_string,
+    .to_string_notation = common_to_string_notation,
+    .get_prototypes = common_get_prototypes,
+    .get_topology = common_get_topology,
+    .get_keys = get_keys,
+    .get_property = get_property,
+    .set_property = set_property_on_immutable,
+    .add = stub_add,
+    .sub = stub_sub,
+    .get_boolean_value = stub_get_boolean_value,
+    .get_integer_value = stub_get_integer_value,
+    .get_real_value = stub_get_real_value,
+    .call = stub_call
+};
+
+/**
+ * @var boolean_proto
+ * @brief The prototype boolean object.
+ * 
+ * This is the prototype boolean object, which is the instance that serves as the 
+ * prototype for all boolean objects.
+ */
+static object_t boolean_proto = {
+    .vtbl = &boolean_proto_vtbl
+};
+
+object_t *get_boolean_proto() {
+    return &boolean_proto;
+}
+
+/**
  * @brief Compares two boolean objects based on their boolean values.
  * @param obj1 The first object to compare.
  * @param obj2 The second object to compare.
@@ -58,30 +128,52 @@ static string_value_t true_to_string_notation(const object_t *obj) {
 }
 
 /**
- * @brief Retrieves all property keys from an object (stub implementation).
+ * @var prototypes
+ * @brief Array of prototypes for the boolean object.
  * 
- * This is a stub implementation of the function to retrieve all keys of the properties 
- * defined on an object. Currently, it returns an empty `object_array_t` as a placeholder.
- * 
- * @param obj The object from which to retrieve the keys.
- * @return An empty `object_array_t` (placeholder implementation).
+ * It contains only the `boolean_proto` prototype.
  */
-static object_array_t get_keys(const object_t *obj) {
-    return (object_array_t){ NULL, 0 };
+static object_t* prototypes[] = {
+    &boolean_proto
+};
+
+/**
+ * @brief Retrieves the prototypes of a boolean object.
+ * 
+ * This function returns an array of prototypes for a boolean object.
+ * In this case, it contains only the boolean prototype.
+ * 
+ * @param obj The object whose prototypes are to be retrieved.
+ * @return An object_array_t containing the prototypes of the boolean object.
+ */
+static object_array_t get_prototypes(const object_t *obj) {
+    object_array_t result = {
+        .items = prototypes,
+        .size = 1
+    };
+    return result;
 }
 
 /**
- * @brief Retrieves the value of a property from an object (stub implementation).
+ * @brief Retrieves the full prototype topology of a boolean object.
  * 
- * This is a stub implementation of the function to retrieve the value of a property from
- * an object. Currently, it returns `NULL` as a placeholder.
+ * This function returns the full prototype chain (topology) of a boolean object.
+ * The topology includes the `boolean_proto` prototype and the root object.
  * 
- * @param obj The object from which to retrieve the property.
- * @param key The key of the property to retrieve.
- * @return Always returns `NULL` (placeholder implementation).
+ * @param obj The object whose prototype topology is to be retrieved.
+ * @return An object_array_t containing the full prototype chain.
  */
-static object_t *get_property(const object_t *obj, const object_t *key) {
-    return NULL;
+static object_array_t get_topology(const object_t *obj) {
+    static object_t* topology[2] = {0};
+    if (topology[0] == NULL) {
+        topology[0] = &boolean_proto;
+        topology[1] = get_root_object();
+    }
+    object_array_t result = {
+        .items = topology,
+        .size = 2
+    };
+    return result;
 }
 
 /**
@@ -117,6 +209,8 @@ static object_vtbl_t false_vtbl = {
     .clone = clone_singleton,
     .to_string = false_to_string,
     .to_string_notation = false_to_string_notation,
+    .get_prototypes = get_prototypes,
+    .get_topology = get_topology,
     .get_keys = get_keys,
     .get_property = get_property,
     .set_property = set_property_on_immutable,
@@ -150,6 +244,8 @@ static object_vtbl_t true_vtbl = {
     .clone = clone_singleton,
     .to_string = true_to_string,
     .to_string_notation = true_to_string_notation,
+    .get_prototypes = get_prototypes,
+    .get_topology = get_topology,
     .get_keys = get_keys,
     .get_property = get_property,
     .set_property = set_property_on_immutable,
